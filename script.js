0fd32e6e9678e9c79f22ebc1e874965a1c3e7dab
Katrine Meyer <katrinemeyer@Katrines-MacBook-Pro.local>
2019-12-16 13:03:07 +0100

    tilføjet nav og klik_produkt

--- script.js
+++ script.js
@@ -46,3 +46,33 @@ window.onclick = function (event) {
 
 
 // produktview slut
+
+//burger menu
+//hvad starter js med?
+window.addEventListener("load", startVises);
+
+// Hvad skal i skrive for at skjule den div?
+
+function startVises() {
+    console.log("startVises");
+    document.querySelector("#menu").addEventListener("click", clickMenuKnap);
+
+}
+
+function clickMenuKnap() {
+    console.log("clickMenuKnap");
+    document.querySelector("#menu").classList.remove("hidden");
+
+    document.querySelector("#menuknap").classList.add("hide");
+    document.querySelector("#lukkeknap").classList.remove("hide");
+    document.querySelector("#lukkeknap").addEventListener("click", clickMenuVoek);
+
+
+}
+
+function clickMenuVoek() {
+    console.log("clickMenuVoek");
+    document.querySelector("#lukkeknap").classList.add("hide");
+    document.querySelector("#menuknap").classList.remove("hide");
+    document.querySelector("#menu").classList.add("hidden");
+}
